2a697eaaee0da9ee3ab67d10a9d8c3a575981d59
unknown <dcbarca11@gmail.com>
2018-10-07 14:29:32 -0700

    Mapped the names on the code to the robot

--- src/main/java/org/firstinspires/ftc/teamcode/robot/DriveTrain.java
+++ src/main/java/org/firstinspires/ftc/teamcode/robot/DriveTrain.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by Dravid-C on 10/7/2018.
  *
  */
 public class DriveTrain {
     private Robot robot = null;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
 
     public DriveTrain(Robot inRobot){
         robot = inRobot;
+
+        leftDrive = robot.hardwareMap.get(DcMotor.class,"left_drive");
+        rightDrive = robot.hardwareMap.get(DcMotor.class, "right_drive");
     }
 }
